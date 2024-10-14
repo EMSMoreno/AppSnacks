77cd58c3ba873b97084150597adfaf369b0083bf
EMSMoreno <emsmoreno97@gmail.pt>
2024-10-14 18:41:24 +0100

    HomePage

--- Pages/HomePage.xaml.cs
+++ Pages/HomePage.xaml.cs
@@ -1,9 +1,163 @@
+using AppSnacks.Models;
+using AppSnacks.Services;
+using AppSnacks.Validators;
+using SnacksApp.Pages;
+
 namespace AppSnacks.Pages;
 
 public partial class HomePage : ContentPage
 {
-	public HomePage()
+    private readonly ApiService _apiService;
+    private readonly IValidator _validator;
+    private bool _loginPageDisplayed = false;
+
+    public HomePage(ApiService apiService, IValidator validator)
 	{
 		InitializeComponent();
-	}
+        LblUserName.Text = "Hello, " + Preferences.Get("userName", string.Empty);
+        _apiService = apiService;
+        _validator = validator;
+        Title = AppConfig.titleHomePage;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await GetCategoriesList();
+        await GetBestSellers();
+        await GetPopular();
+    }
+
+    private async Task<IEnumerable<Category>> GetCategoriesList()
+    {
+        try
+        {
+            var (categories, errorMessage) = await _apiService.GetCategories();
+
+            if (errorMessage == "Unauthorized" && !_loginPageDisplayed)
+            {
+                await DisplayLoginPage();
+                return Enumerable.Empty<Category>();
+            }
+
+            if (categories == null)
+            {
+                await DisplayAlert("Error", errorMessage ?? "Unable to retrieve categories.", "OK");
+                return Enumerable.Empty<Category>();
+            }
+
+            CvCategories.ItemsSource = categories;
+            return categories;
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", $"An unexpected error occurred: {ex.Message}", "OK");
+            return Enumerable.Empty<Category>();
+        }
+    }
+
+    private async Task DisplayLoginPage()
+    {
+        _loginPageDisplayed = true;
+        await Navigation.PushAsync(new LoginPage(_apiService, _validator));
+    }
+
+    private async Task<IEnumerable<Product>> GetBestSellers()
+    {
+        try
+        {
+            var (products, errorMessage) = await _apiService.GetProducts("bestseller", string.Empty);
+
+            if (errorMessage == "Unauthorized" && !_loginPageDisplayed)
+            {
+                await DisplayLoginPage();
+                return Enumerable.Empty<Product>();
+            }
+
+            if (products == null)
+            {
+                await DisplayAlert("Error", errorMessage ?? "Unable to retrieve categories.", "OK");
+                return Enumerable.Empty<Product>();
+            }
+
+            CvBestSellers.ItemsSource = products;
+            return products;
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", $"An unexpected error occurred: {ex.Message}", "OK");
+            return Enumerable.Empty<Product>();
+        }
+    }
+
+    private async Task<IEnumerable<Product>> GetPopular()
+    {
+        try
+        {
+            var (products, errorMessage) = await _apiService.GetProducts("popular", string.Empty);
+
+            if (errorMessage == "Unauthorized" && !_loginPageDisplayed)
+            {
+                await DisplayLoginPage();
+                return Enumerable.Empty<Product>();
+            }
+
+            if (products == null)
+            {
+                await DisplayAlert("Error", errorMessage ?? "Unable to retrieve categories.", "OK");
+                return Enumerable.Empty<Product>();
+            }
+            CvPopular.ItemsSource = products;
+            return products;
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", $"An unexpected error occurred: {ex.Message}", "OK");
+            return Enumerable.Empty<Product>();
+        }
+    }
+
+    private void CvBestSellers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (sender is CollectionView collectionView)
+        {
+            NavigateToProductDetailsPage(collectionView, e);
+        }
+    }
+
+    private void CvPopular_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (sender is CollectionView collectionView)
+        {
+            NavigateToProductDetailsPage(collectionView, e);
+        }
+    }
+
+    private void NavigateToProductDetailsPage(CollectionView collectionView, SelectionChangedEventArgs e)
+    {
+        var currentSelection = e.CurrentSelection.FirstOrDefault() as Product;
+
+        if (currentSelection == null)
+            return;
+
+        Navigation.PushAsync(new ProductDetailsPage(
+            currentSelection.Id, currentSelection.Name!, _apiService, _validator));
+
+        collectionView.SelectedItem = null;
+    }
+
+    private void CvCategories_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var currentSelection = e.CurrentSelection.FirstOrDefault() as Category;
+
+        if (currentSelection == null) return;
+
+
+        Navigation.PushAsync(new ProductsListPage(currentSelection.Id,
+                                                     currentSelection.Name!,
+                                                     _apiService,
+                                                     _validator));
+
+        ((CollectionView)sender).SelectedItem = null;
+    }
 }
