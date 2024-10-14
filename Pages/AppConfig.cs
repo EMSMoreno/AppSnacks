3aad0112b6ba697ea1d59b56dd8e73d2191ac17d
EMSMoreno <emsmoreno97@gmail.pt>
2024-10-14 16:14:53 +0100

    ProductsPage

---
+++ Pages/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace AppSnacks.Pages
+{
+    public class AppConfig
+    {
+        public static readonly string BaseUrl = "https://rdv5rms4-44326.uks1.devtunnels.ms/";
+
+        public static readonly string titleHomePage = "Edu Snacks";
+    }
+}
