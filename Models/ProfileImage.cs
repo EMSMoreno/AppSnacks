2c2f46c6093df5fe90e439bccb8b3e4b7b7c1b44
EMSMoreno <emsmoreno97@gmail.pt>
2024-10-17 14:51:42 +0100

    OrdersDetailPage & ProfilePage

---
+++ Models/ProfileImage.cs
@@ -0,0 +1,8 @@
+﻿namespace AppSnacks.Models
+{
+    public class ProfileImage
+    {
+        public string? UrlImage { get; set; }
+        public string? ImagePath => AppConfig.BaseUrl + UrlImage;
+    }
+}
